29d279eb7f24128afa8f6d543bd09b79d0f50b43
Fernando Garioli <f.ribeirogarioli@gmail.com>
2019-06-03 10:42:52 -0300

    fix: mensagem de erro no login e reset no form

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,11 +3,8 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import {
   LoadingController,
-  ToastController,
-  NavController
+  AlertController
 } from "@ionic/angular";
-
-import { TurmAlunDTO } from "../../models/turmalun.dto";
 import { AuthServiceProvider } from "../../services/auth.service";
 
 @Component({
@@ -19,12 +16,11 @@ export class LoginPage implements OnInit {
   formGroup: FormGroup;
 
   constructor(
-    public router: Router,
+    private router: Router,
     private loadindCtrl: LoadingController,
     private authService: AuthServiceProvider,
     private formBuilder: FormBuilder,
-    private toastController: ToastController,
-    private nav: NavController
+    private alertController: AlertController
   ) {}
 
   ngOnInit() {
@@ -36,33 +32,42 @@ export class LoginPage implements OnInit {
   }
 
   async login() {
+    let loading = null;
     try {
-      let loading = await this.loadindCtrl.create({
+      loading = await this.loadindCtrl.create({
         message: "Realizando Login"
       });
 
       await loading.present();
 
       await this.authService.login(this.formGroup.value);
-
-      if (this.authService.getRole() == "ROLE_RESP") {
-        // this.nav.navigateForward(`/alunos/${this.authService.getUserData().codUsuario}`);
-        this.nav.navigateForward(`/alunos`);
+      this.formGroup.reset();
+      
+      if (this.authService.getRole() == "ROLE_RESP") {        
+        this.router.navigate(["alunos"]);
       } else if (this.authService.getRole() == "ROLE_ALUN") {
-        this.nav.navigateForward(
+        this.router.navigate([
           `/aluno/${this.authService.getUserData().codUsuario}`
-        );
+        ]);
       }
 
       await loading.dismiss();
     } catch (error) {
-      console.log(error);
-      const toast = await this.toastController.create({
-        message: "Erro ao realizar Login",
-        duration: 2000,
-        position: "top"
+      await loading.dismiss();
+      let err = JSON.parse(error.error);
+      let hdr = err.error;
+      let msg = err.message;
+      const alert = await this.alertController.create({
+        header: hdr,
+        message: msg,
+        buttons: [
+          {
+            text: "Ok"
+          }
+        ]
       });
-      await toast.present();
+
+      await alert.present();
     }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,75 +26,62 @@ export class AuthServiceProvider {
     private helper: JwtHelperService
   ) {
     this.platform.ready().then(() => {
-      // console.log("Check Token!!!!!!!");
       this.checkToken();
     });
   }
 
   async login(cred: CredenciaisDTO) {
-    try {
-      let res = await this.http
-        .post(
-          `${environment.api}/login`,
-          {},
-          {
-            headers: new HttpHeaders().set(
-              "Content-Type",
-              "application/x-www-form-urlencoded"
-            ),
-            params: {
-              username: cred.username,
-              password: cred.password,
-              grant_type: "access_token"
-            },
-            observe: "response",
-            responseType: "text"
-          }
-        )
-        .toPromise();
+    let res = await this.http
+      .post(
+        `${environment.api}/login`,
+        {},
+        {
+          headers: new HttpHeaders().set(
+            "Content-Type",
+            "application/x-www-form-urlencoded"
+          ),
+          params: {
+            username: cred.username,
+            password: cred.password,
+            grant_type: "access_token"
+          },
+          observe: "response",
+          responseType: "text"
+        }
+      )
+      .toPromise();
 
-      await this.sucessfullLogin(res.headers.get("Authorization"));
-    } catch (error) {
-      console.log(error);
-    }
+    await this.sucessfullLogin(res.headers.get("Authorization"));
   }
 
   async refreshToken() {
-    try {
-      let res = await this.http
-        .post(
-          `${environment.api}/login`,
-          {},
-          {
-            headers: new HttpHeaders().set(
-              "Content-Type",
-              "application/x-www-form-urlencoded"
-            ),
-            params: { grant_type: "refresh_token" },
-            observe: "response",
-            responseType: "text"
-          }
-        )
-        .toPromise();
+    let res = await this.http
+      .post(
+        `${environment.api}/login`,
+        {},
+        {
+          headers: new HttpHeaders().set(
+            "Content-Type",
+            "application/x-www-form-urlencoded"
+          ),
+          params: { grant_type: "refresh_token" },
+          observe: "response",
+          responseType: "text"
+        }
+      )
+      .toPromise();
 
-      await this.sucessfullLogin(res.headers.get("Authorization"));
-    } catch (error) {
-      console.log(error);
-    }
+    await this.sucessfullLogin(res.headers.get("Authorization"));
   }
 
   private async sucessfullLogin(authValue: string) {
-    try {
-      authValue = authValue.substring(7);
-      await this.storage.set(TOKEN_KEY, authValue);
+    authValue = authValue.substring(7);
+    await this.storage.set(TOKEN_KEY, authValue);
 
-      let decoded = this.helper.decodeToken(authValue);
-      this.user = decoded["user_data"];
-      this.role = decoded["authorities"][0]["authority"];
-      this.authState.next(true);
-    } catch (error) {
-      console.log(error);
-    }
+    let decoded = this.helper.decodeToken(authValue);
+    this.user = decoded["user_data"];
+    this.role = decoded["authorities"][0]["authority"];
+    this.authState.next(true);
   }
 
   async logout() {
@@ -103,22 +90,18 @@ export class AuthServiceProvider {
   }
 
   async checkToken() {
-    try {
-      let token = await this.storage.get(TOKEN_KEY);
+    let token = await this.storage.get(TOKEN_KEY);
 
-      if (token) {
-        let decoded = this.helper.decodeToken(token);
-        let isExpired = this.helper.isTokenExpired(token);
+    if (token) {
+      let decoded = this.helper.decodeToken(token);
+      let isExpired = this.helper.isTokenExpired(token);
 
-        if (!isExpired) {
-          this.user = decoded;
-          this.authState.next(true);
-        } else {
-          await this.storage.remove(TOKEN_KEY);
-        }
+      if (!isExpired) {
+        this.user = decoded;
+        this.authState.next(true);
+      } else {
+        await this.storage.remove(TOKEN_KEY);
       }
-    } catch (error) {
-      console.log(error);
     }
   }
 
