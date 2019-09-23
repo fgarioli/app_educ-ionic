6ab05558833b2a84e2944484c7648f16edbf8a4d
Fernando Garioli <f.ribeirogarioli@gmail.com>
2019-09-23 11:20:49 -0300

    atualizando servidor de producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  api: "http://producao.cachoeiro.es.gov.br:8084/app_educ-rest",
-  whitelistedDomains: ["producao.cachoeiro.es.gov.br:8084"],
+  api: "http://producao.cachoeiro.es.gov.br:8082/app_educ-rest",
+  whitelistedDomains: ["producao.cachoeiro.es.gov.br:8082"],
   blacklistedRoutes: [
-    "producao.cachoeiro.es.gov.br:8084/app_educ-rest/login"
+    "producao.cachoeiro.es.gov.br:8082/app_educ-rest/login"
   ],
   ano: "2019"
 };
